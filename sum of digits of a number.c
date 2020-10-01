14f0559091149bcf0a272dc19b33a02f6f20dc5a
Rupesh Ahuja <55542613+Rupesh-0001@users.noreply.github.com>
2020-10-01 18:16:32 +0530

    Create sum of digits of a number.c

---
+++ sum of digits of a number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int DigitSum(int num);
+ 
+int main()
+{
+    int n1, sum;
+	printf("\n\n Recursion : Find the sum of digits of a number :\n");
+	printf("-----------------------------------------------------\n");     
+    printf(" Input any number to find sum of digits: ");
+    scanf("%d", &n1);
+     
+    sum = DigitSum(n1);//call the function for calculation
+     
+    printf(" The Sum of digits of %d = %d\n\n", n1, sum);
+     
+    return 0;
+}
+
+int DigitSum(int n1)
+{
+    if(n1 == 0)
+        return 0;
+         
+    return ((n1 % 10) + DigitSum(n1 / 10));//calling the function DigitSum itself
+}
