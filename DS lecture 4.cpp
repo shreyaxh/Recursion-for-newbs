f13458e5ba92df4019480d06f6e84237e354fc1b
Shreyash Pankaj <64730654+shreyaxh@users.noreply.github.com>
2020-10-01 13:40:18 +0530

    Recursion Question w/ Solution by shreyaxh

---
+++ DS lecture 4.cpp
@@ -0,0 +1,69 @@
+A(3)
+{
+	A(2)
+	{
+		A(1)
+		{
+			A(0)
+			{
+				exit braces
+			}
+			print 1
+			A(0)
+			{
+				exit braces
+			}
+			exit braces
+		}
+		print 2
+		A(1)
+		{
+			A(0)
+			{
+				exit braces
+			}
+			print 1
+			A(0)
+			{
+				exit braces
+			}
+			exit braces
+		}
+		exit braces
+	}
+	print 3
+	A(2)
+	{
+		A(1)
+		{
+			A(0)
+			{
+				exit braces
+			}
+			print 1
+			A(0)
+			{
+				exit braces
+			}
+			exit braces
+		}
+		print 2
+		A(1)
+		{
+			A(0)
+			{
+				exit braces
+			}
+			print 1
+			A(0)
+			{
+				exit braces
+			}
+			exit braces
+		}
+		exit braces
+	}
+	exit braces
+}
+
+Hence Output : 1 2 1 3 1 2 1
